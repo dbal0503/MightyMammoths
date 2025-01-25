cdb0f4ef5ed3a5cdc2b25a8a851f736c4ff85f95
JustinSciortino <justinsciortino28@gmail.com>
2025-01-25 13:16:48 -0500

    linter and e2e

--- frontend/MightyMammoths/.eslintrc.js
+++ frontend/MightyMammoths/.eslintrc.js
@@ -1,5 +1,24 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'plugin:jest/recommended'],
   ignorePatterns: ['/dist/*'],
+  env: {
+    jest: true
+  },
+  plugins: ['jest'],
+  globals: {
+    device: 'readonly',
+    element: 'readonly',
+    by: 'readonly',
+    expect: 'readonly',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/e2e/**/*.js'],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
+
