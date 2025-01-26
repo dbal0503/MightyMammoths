076d780712c910c2932252c999671e4b871a0cc0
JustinSciortino <justinsciortino28@gmail.com>
2025-01-26 12:45:17 -0500

    e2e

--- frontend/MightyMammoths/e2e/starter.test.js
+++ frontend/MightyMammoths/e2e/starter.test.js
@@ -10,12 +10,4 @@ describe('Example', () => {
   it('should have welcome screen', async () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
-
-  it('Should show step 2 on the welcome screen', async () => {
-    await expect(element(by.id('step2'))).toBeVisible();
-  });
-
-  it('Should show step 3 on the welcome screen', async () => {
-    await expect(element(by.id('step3'))).toBeVisible();
-  });
 });
