d0060ed69391feaeac55ffe9d734244d996b8932
JustinSciortino <justinsciortino28@gmail.com>
2025-01-25 20:44:24 -0500

    config

--- frontend/MightyMammoths/.detoxrc.js
+++ frontend/MightyMammoths/.detoxrc.js
@@ -9,15 +9,28 @@ module.exports = {
     }
   },
   apps: {
+    'ios.debug': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/mightymammoths.app',
+      build: 'xcodebuild -workspace ios/mightymammoths.xcworkspace -scheme mightymammoths -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+    },
     'ios.release': {
       type: 'ios.app',
-      binaryPath: 'path/to/your/expo-built-app/MightyMammoths.app',
-      build: 'expo prebuild && expo run:ios --no-wait',
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/mightymammoths.app',
+      build: 'xcodebuild -workspace ios/mightymammoths.xcworkspace -scheme mightymammoths -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+    },
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [
+        8081
+      ]
     },
     'android.release': {
       type: 'android.apk',
-      binaryPath: 'path/to/your/expo-built-app/MightyMammoths.apk',
-      build: 'expo prebuild && expo run:android --variant release',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
     }
   },
   devices: {
