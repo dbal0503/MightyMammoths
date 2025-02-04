9fef47e454ea9b9206f6f17dbee766476c409c67
TheHalpinator <144919217+TheHalpinator@users.noreply.github.com>
2025-02-03 19:58:27 -0500

    updated transportation list
    made the choices buttons that assign their mode to the transportation variable

---
+++ frontend/MightyMammoths/components/Destinations.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { IconSymbol } from '@/components/ui/IconSymbol'
+
+export function DestinationChoices() {
+    
+    return (
+        <View style={styles.container}>
+            {/* Route Heading */}
+            
+        </View>
+        );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '20%',
+        padding: 16,
+        marginBottom:0,
+        borderBottomLeftRadius:10,
+        borderBottomRightRadius:10,
+        backgroundColor: 'black'
+      },
+});
