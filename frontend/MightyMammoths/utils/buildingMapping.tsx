6fb06294ffbfcebc96d017492990b0d47ceccde1
Vito <vito.rizzuto190@gmail.com>
2025-02-05 22:04:23 -0500

    displaying multiple route options - addding smith building (LC)

--- frontend/MightyMammoths/utils/buildingMapping.tsx
+++ frontend/MightyMammoths/utils/buildingMapping.tsx
@@ -5,6 +5,8 @@ export function getBuildingAddress(abbreviation: string): string {
     JMSB: "Concordia University, John Molson School of Business, Montreal, QC",
     "CL Building": "Concordia University, CL Building, Montreal, QC",
     "Learning Square": "Concordia University, Learning Square, Montreal, QC",
+    "Smith Building":
+      "Concordia University Smith Building, Loyola Campus, Montreal, QC, Canada",
   };
 
   return mapping[abbreviation] || abbreviation;
