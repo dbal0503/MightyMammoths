2cca52d8cd32d33c7a2f99e45e3159506f456c10
JustinSciortino <justinsciortino28@gmail.com>
2025-02-06 13:46:55 -0500

    new branch with changes

---
+++ frontend/MightyMammoths/utils/getNextShuttleBus.tsx
@@ -0,0 +1,48 @@
+import busSchedule from "./shuttleBusSchedule.json"; 
+
+
+interface BusSchedule {
+  [day: string]: {
+    [campus: string]: string[];
+  };
+}
+
+function timeToNumber(time: string): number {
+    const [hours, minutes] = time.split(":").map(Number);
+    return hours * 100 + minutes;
+  }
+
+export function findNextBusTime(day: string, campus: string, currentTime: string): string | null {
+  const schedule: string[] | undefined = (busSchedule as BusSchedule)[day]?.[campus];
+
+  // Handles the case for when day is Sunday or Saturday
+  if (!schedule) {
+    console.error("Invalid day or campus");
+    return null;
+  }
+
+  const currentTimeNum = timeToNumber(currentTime);
+
+  let left = 0;
+  let right = schedule.length - 1;
+
+  const firstBusNum = timeToNumber(schedule[left]);
+  const lastBusNum = timeToNumber(schedule[right]);
+
+  if (currentTimeNum < firstBusNum || currentTimeNum > lastBusNum) {
+    return null;
+  }
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midTimeNum = timeToNumber(schedule[mid]);
+
+    if (midTimeNum >= currentTimeNum) {
+      right = mid - 1;
+    } else {
+      left = mid + 1; 
+    }
+  }
+
+  return schedule[left] || "No more buses today";
+}
