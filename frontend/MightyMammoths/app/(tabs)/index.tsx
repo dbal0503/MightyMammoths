7ca142cdfa2b04fb9bb4b2ea97c8f68b087acc38
Yan P <y.pilon9@gmail.com>
2025-02-06 19:37:37 -0500

    replaced bottom sheet with action sheet

--- frontend/MightyMammoths/app/(tabs)/index.tsx
+++ frontend/MightyMammoths/app/(tabs)/index.tsx
@@ -2,22 +2,17 @@ import React, { useCallback, useRef, useMemo, useState, useEffect } from "react"
 import { StyleSheet, View, Text, Button, KeyboardAvoidingView, Keyboard  } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
-import ActionSheet from "react-native-actions-sheet"; //for some reason if I try to import it along ActionSheetRef it throws an error lol
-import { ActionSheetRef } from "react-native-actions-sheet";
+import LoyolaSGWToggleSheet from "@/components/ui/sheet/LoyolaSGWToggleSheet";
 
-import ToggleSwitch from "@/components/ui/input/ToggleSwitch";
-import GoogleCalendarButton from "@/components/ui/input/GoogleCalendarButton";
-import RetroSwitch from "@/components/ui/input/RetroSwitch";
+import '@/components/Sheets'
+
 import BuildingDropdown from "@/components/ui/input/BuildingDropdown";
 import RoundButton from "@/components/ui/buttons/RoundButton";
 
 export default function HomeScreen() {
   const sheetRef = useRef<BottomSheet>(null);
-  const actionSheetRef = useRef<ActionSheetRef>(null);
   const snapPoints = useMemo(() => ["17%", "70%"], []);
   const [selectedCampus, setSelectedCampus] = useState("SGW");
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
  
   useEffect(() => {
@@ -53,39 +48,22 @@ export default function HomeScreen() {
         </View>
         <View style={styles.bottomElements}>
           <RoundButton imageSrc={require('@/assets/images/recenter-map.png')} onPress={()=>{
-            actionSheetRef.current?.hide()
+            
           }} /> 
         </View>
-        <BottomSheet
-          ref={sheetRef}
-          snapPoints={snapPoints}
-          enableDynamicSizing={false}
-          backgroundStyle={{backgroundColor: '#010213'}}
-          handleIndicatorStyle={{backgroundColor: 'white'}}
-        >
-        <View style={styles.centeredView}>
-          <ToggleSwitch
-            options={["SGW", "LOY"]}
-            onToggle={(selected) => setSelectedCampus(selected)}
-          />
-        </View>
-          <Text style={styles.subTitleText}>Calendar</Text>
-          <GoogleCalendarButton />
-          <Text style={styles.subTitleText}>Accessibility</Text>
-          <View style={styles.accessibilityContainer}>
-            <Text style={styles.accessibilityLabel}>Accessibility mode</Text>
-            <RetroSwitch value={isEnabled} onValueChange={setIsEnabled} />
-          </View>
-        </BottomSheet>
-        <ActionSheet ref={actionSheetRef}>
-          <Text>Hi, I am here.</Text>
-        </ActionSheet>
+        <LoyolaSGWToggleSheet
+          setSelectedCampus={setSelectedCampus}
+        />
       </GestureHandlerRootView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  root:{
+    backgroundColor: '#010213',
+    borderRadius: 10
+  },
   container: {
     flex: 1,
     paddingTop: 70,
---
+++ frontend/MightyMammoths/components/ui/sheets/LoyolaSGWToggleSheet.tsx
@@ -0,0 +1,94 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+
+import GoogleCalendarButton from '../input/GoogleCalendarButton';
+import ActionSheet from 'react-native-actions-sheet';
+import ToggleSwitch from '../input/ToggleSwitch';
+import RetroSwitch from '../input/RetroSwitch';
+import { ActionSheetProps } from 'react-native-actions-sheet';
+import {ActionSheetRef, useSheetRef} from "react-native-actions-sheet";
+
+export type LoyolaSGWToggleSheetProps = ActionSheetProps & {
+    setSelectedCampus: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function LoyolaSGWToggleSheet({
+    isModal = false,
+    snapPoints = [17, 70],
+    setSelectedCampus,
+    backgroundInteractionEnabled = true,
+    closable = false,
+    gestureEnabled = true,
+    initialSnapIndex = 0,
+    overdrawEnabled = false,
+    overdrawSize = 200,
+}: LoyolaSGWToggleSheetProps) {
+    const actionSheetRef = useRef<ActionSheetRef>(null);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    useEffect(()=>{
+        actionSheetRef.current?.show();
+    },[]);
+
+    return (
+      <ActionSheet
+        ref={actionSheetRef}
+        isModal={isModal} 
+        snapPoints={snapPoints} 
+        backgroundInteractionEnabled={backgroundInteractionEnabled}
+        closable={closable}
+        gestureEnabled={gestureEnabled}
+        initialSnapIndex={initialSnapIndex}
+        containerStyle={styles.root}
+        overdrawEnabled={overdrawEnabled}
+        overdrawSize={overdrawSize}
+        >
+          <View style={styles.centeredView}>
+            <ToggleSwitch
+            options={["SGW", "LOY"]}
+            onToggle={(selected) => setSelectedCampus(selected)}
+          />
+          </View>
+          <Text style={styles.subTitleText}>Calendar</Text>
+          <GoogleCalendarButton />
+          <Text style={styles.subTitleText}>Accessibility</Text>
+          <View style={styles.accessibilityContainer}>
+            <Text style={styles.accessibilityLabel}>Accessibility mode</Text>
+            <RetroSwitch value={isEnabled} onValueChange={(value)=>{
+                setIsEnabled(value)        
+            }} />
+          </View>
+      </ActionSheet>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    root:{
+        backgroundColor: '#010213',
+        borderRadius: 10
+    },
+    centeredView: {
+        marginTop: "10%",
+        alignItems: "center",
+        justifyContent: 'flex-start',
+      },
+      subTitleText : {
+        color: "#b2b3b8",
+        fontSize: 16,
+        marginLeft: 40,
+        marginTop: 30,
+      },
+      accessibilityContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginLeft: 40,
+        marginRight: 40,
+        marginTop: 20,
+      },
+      accessibilityLabel: {
+        color: "white",
+        fontSize: 22,
+      },
+  });
+   
+  export default LoyolaSGWToggleSheet;
