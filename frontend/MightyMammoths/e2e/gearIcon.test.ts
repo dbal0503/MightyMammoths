ee71b532a4a038421cab600fc7234553f9970afb
JustinSciortino <justinsciortino28@gmail.com>
2025-02-12 13:32:13 -0500

    testing end to end tests

--- frontend/MightyMammoths/e2e/gearIcon.test.ts
+++ frontend/MightyMammoths/e2e/gearIcon.test.ts
@@ -5,14 +5,14 @@ describe('Gear Icon visible', () => {
     await device.launchApp({
       permissions: { location: 'always' } 
     });
-    await new Promise(resolve => setTimeout(resolve, 5000));  
+    
   });
 
   it('should display gear icon', async () => {
     console.log("Checking if gear icon exists...");
     await waitFor(element(by.id('gear-icon')))
-      .toExist()
-      .withTimeout(40000);
+      .toBeVisible()
+      .withTimeout(60000);
       console.log("Gear icon exists, checking visibility...");
     await expect(element(by.id('gear-icon'))).toBeVisible();
   });
