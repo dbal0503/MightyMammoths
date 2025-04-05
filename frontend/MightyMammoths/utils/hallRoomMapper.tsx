883bf692c56d7dd3155e4d54d547ff40e44fa57c
Vito <vito.rizzuto190@gmail.com>
2025-04-05 18:41:26 -0400

    Util functions for finding room, encoded room and floor...

---
+++ frontend/MightyMammoths/utils/hallRoomMapper.tsx
@@ -0,0 +1,44 @@
+const { rooms } = require("./rooms.json");
+
+interface Room {
+  roomNumber: string;
+  encodedId: string;
+  floor: string;
+}
+
+interface RoomsData {
+  rooms: Room[];
+}
+
+const roomsData: RoomsData = {
+  rooms: rooms,
+};
+
+/**
+ * Find a room by its room number
+ * @param roomNumber The room number to search for
+ * @returns The room object if found, undefined otherwise
+ */
+export const findRoomByNumber = (roomNumber: string): Room | undefined => {
+  return roomsData.rooms.find((room) => room.roomNumber === roomNumber);
+};
+
+/**
+ * Get the encoded ID for a room by its room number
+ * @param roomNumber The room number to search for
+ * @returns The encoded ID if found, undefined otherwise
+ */
+export const getRoomEncodedId = (roomNumber: string): string | undefined => {
+  const room = findRoomByNumber(roomNumber);
+  return room?.encodedId;
+};
+
+/**
+ * Get the floor for a room by its room number
+ * @param roomNumber The room number to search for
+ * @returns The floor if found, undefined otherwise
+ */
+export const getRoomFloor = (roomNumber: string): string | undefined => {
+  const room = findRoomByNumber(roomNumber);
+  return room?.floor;
+};
